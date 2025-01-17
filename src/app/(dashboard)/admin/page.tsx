749b3b690eed4a088a14278c5384c095976d7399
sanyammahajan21 <sanyammahajan2003@outlook.com>
2025-01-17 22:14:22 +0530

    Refactor parent references to registrar throughout the application

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,6 @@
 import Announcements from "@/components/Announcements";
 import AttendanceChartContainer from "@/components/AttendanceChartContainer";
-import CountChartContainer from "@/components/CountChartContainer";
 import EventCalendarContainer from "@/components/EventCalendarContainer";
-import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = ({
@@ -23,19 +21,12 @@ const AdminPage = ({
         </div>
         {/* MIDDLE CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
-          {/* COUNT CHART */}
-          <div className="w-full lg:w-1/3 h-[450px]">
-            <CountChartContainer />
-          </div>
           {/* ATTENDANCE CHART */}
           <div className="w-full lg:w-2/3 h-[450px]">
             <AttendanceChartContainer />
           </div>
         </div>
         {/* BOTTOM CHART */}
-        <div className="w-full h-[500px]">
-          <FinanceChart />
-        </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
---
+++ src/app/(dashboard)/list/allotedSubjects/page.tsx
@@ -0,0 +1,9 @@
+const allotedSubjects = () => {
+  return (
+    <div>
+      <h1>Alloted Subjects</h1>
+    </div>
+  );
+};
+
+export default allotedSubjects;
