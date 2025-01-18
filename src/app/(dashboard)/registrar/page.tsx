6aa8b1f27468dee8010c0f4ba2626eeb1b842a97
Tanishkackria13 <tanishkackria@gmail.com>
2025-01-18 11:42:55 +0530

    Registrar page changes and defines some roles in setting

--- src/app/(dashboard)/registrar/page.tsx
+++ src/app/(dashboard)/registrar/page.tsx
@@ -1,36 +1,36 @@
 import Announcements from "@/components/Announcements";
-import BigCalendarContainer from "@/components/BigCalendarContainer";
-import prisma from "@/lib/prisma";
-import { auth } from "@clerk/nextjs/server";
+import AttendanceChartContainer from "@/components/AttendanceChartContainer";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
+import UserCard from "@/components/UserCard";
 
-
-const RegistrarPage = async () => {
-  const { userId } = auth();
-  const currentUserId = userId;
-  
-  const students = await prisma.student.findMany({
-    where: {
-      registrarId: currentUserId!,
-    },
-  });
-
+const RegistrarPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
-    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className="">
-        {students.map((student) => (
-          <div className="w-full xl:w-2/3" key={student.id}>
-            <div className="h-full bg-white p-4 rounded-md">
-              <h1 className="text-xl font-semibold">
-                Schedule ({student.name + " " + student.surname})
-              </h1>
-              <BigCalendarContainer type="classId" id={student.classId} />
-            </div>
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="admin" />
+          <UserCard type="teacher" />
+          <UserCard type="student" />
+          <UserCard type="registrar" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChartContainer />
           </div>
-        ))}
+        </div>
+        {/* BOTTOM CHART */}
       </div>
       {/* RIGHT */}
-      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendarContainer searchParams={searchParams}/>
         <Announcements />
       </div>
     </div>
